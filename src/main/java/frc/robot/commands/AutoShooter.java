fc43431a1454e8d5b31837a2d16e301a5802d3cd
dphagler <dphagler@gmail.com>
2024-03-19 20:11:00 -0400

    b

--- src/main/java/frc/robot/commands/AutoShooter.java
+++ src/main/java/frc/robot/commands/AutoShooter.java
@@ -11,9 +11,6 @@ import frc.robot.subsystems.Shooter;
 public class AutoShooter extends Command {
 
   private final Shooter m_Shooter;
-  //private final Limelight m_Limelight;
-  double shooterSetPoint;
-  double distance;
   double powerLevel;
 
   /** Creates a new AutoShooterRPM. */
@@ -31,47 +28,9 @@ public class AutoShooter extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    distance = RobotContainer.m_Limelight.getDistance();
     powerLevel = RobotContainer.shooterPower.getDouble(1);
-    //KISS
-    if(distance > 137.5 && distance < 200.0){
-      shooterSetPoint = 3000.0;
     
-    }else if(distance > 132.5 && distance < 137.5){
-      shooterSetPoint = 3000.0;
-    }else if(distance > 127.5 && distance < 132.5){
-      shooterSetPoint = 3000.0;
-    }else if(distance > 122.5 && distance < 127.5){
-      shooterSetPoint = 3000.0;
-    } else if(distance > 117.5 && distance < 122.5) {
-      shooterSetPoint = 3000.0;
-    }  else if(distance > 112.5 && distance < 117.5) {
-      shooterSetPoint = 3000.0;
-    } else if(distance > 107.5 && distance < 112.5) {
-      shooterSetPoint = 3000.0;
-    } else if(distance > 102.5 && distance < 107.5) {
-      shooterSetPoint = 3000.0;
-    } else if(distance > 97.5 && distance < 102.5) {
-      shooterSetPoint = 3000.0;
-    } else if(distance > 92.5 && distance < 97.5) {
-      shooterSetPoint = 3000.0;
-    } else if(distance > 87.5 && distance < 92.5) {
-      shooterSetPoint = 3000.0;
-    } else if(distance > 82.5 && distance < 87.5) {
-      shooterSetPoint = 3000.0;
-    } else if(distance > 77.5 && distance < 82.5) {
-      shooterSetPoint = 3000.0;
-    } else if(distance > 72.5 && distance < 77.5) {
-      shooterSetPoint = 3000.0;
-    } else if(distance > 67.5 && distance < 72.5) {
-      shooterSetPoint = 2500.0;
-    } else {
-      shooterSetPoint = 2500.0;
-    }
-
-    m_Shooter.shoot(3000);
-    //m_Shooter.shoot(shooterSetPoint*powerLevel);
-    //System.out.println(m_Limelight.getDistance());
+    m_Shooter.shoot(3500 * powerLevel);
   }
 
   // Called once the command ends or is interrupted.
