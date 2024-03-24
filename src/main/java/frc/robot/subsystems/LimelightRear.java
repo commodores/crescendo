2449941185bca0c08765a083febe69734f9a6e6b
dphagler <dphagler@gmail.com>
2024-03-23 21:37:48 -0400

    er

--- src/main/java/frc/robot/subsystems/LimelightRear.java
+++ src/main/java/frc/robot/subsystems/LimelightRear.java
@@ -5,7 +5,6 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotContainer;
 
 /**
  * 
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -71,8 +73,14 @@ public class Shooter extends SubsystemBase {
    * the NEO's velocity. Intended to be called when a button is pressed.
    */
   public void shoot(double setPoint) {
-    shooterPIDLeft.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
-    shooterPIDRight.setReference(setPoint-1000, CANSparkMax.ControlType.kVelocity);
+    if(DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get().equals(Alliance.Red)){
+      shooterPIDLeft.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
+      shooterPIDRight.setReference(setPoint-1000, CANSparkMax.ControlType.kVelocity);
+    } else {
+      shooterPIDLeft.setReference(setPoint-1000, CANSparkMax.ControlType.kVelocity);
+      shooterPIDRight.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
+    }
+    
   }
 
   public void shootClose(double setPoint) {
